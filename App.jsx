51100f1ecec5982c598e188182f7e6995af622fe
pawelduda <pawelduda91@gmail.com>
2016-05-24 20:35:16 +0200

    Complete lesson

--- App.jsx
+++ App.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: [
+        { id: 1, name: 'Pawel', age: 25 },
+        { id: 1, name: 'John', age: 24 },
+        { id: 1, name: 'Test', age: 20 }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-        Hello World!
+        <Header />
+        <table>
+          <tbody>
+            {this.state.data.map((person, i) => <TableRow key={i} data={person} />)}
+          </tbody>
+        </table>
       </div>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    );
+  }
+}
+
+class TableRow extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
     );
   }
 }
