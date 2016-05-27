3cb1fceae2bbb954142bd24115dd87ba80d6499e
pawelduda <pawelduda91@gmail.com>
2016-05-27 15:47:37 +0200

    Finish lesson

--- App.jsx
+++ App.jsx
@@ -1,38 +1,46 @@
 import React from 'react';
+let { PropTypes } = React;
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      headerProp: 'Default header',
-      contentProp: 'Default content'
-    }
-  }
-
   render() {
+    let { props } = this;
+    let { objectName1, objectName2, objectName3 } = props.propObject;
+
     return (
       <div>
-        <Header headerProp={this.state.headerProp} />
-        <Content contentProp={this.state.contentProp} />
+        <h3>Array: {props.propArray}</h3>
+        <h3>Bool: {props.propBool ? 'True' : 'False'}</h3>
+        <h3>Func: {props.propFunc(3)}</h3>
+        <h3>Number: {props.propNumber}</h3>
+        <h3>String: {props.propString}</h3>
+        <h3>Object: {objectName1}</h3>
+        <h3>Object: {objectName2}</h3>
+        <h3>Object: {objectName3}</h3>
       </div>
     );
   }
 }
 
-class Header extends React.Component {
-  render() {
-    return (
-      <h1>{this.props.headerProp}</h1>
-    )
-  }
+App.propTypes = {
+  propArray: PropTypes.array.isRequired,
+  propBool: PropTypes.bool.isRequired,
+  propFunc: PropTypes.func,
+  propNumber: PropTypes.number,
+  propString: PropTypes.string,
+  propObject: PropTypes.object
 }
 
-class Content extends React.Component {
-  render() {
-    return (
-      <h2>{this.props.contentProp}</h2>
-    )
+App.defaultProps = {
+  propArray: [1, 2, 3, 4, 5],
+  propBool: true,
+  propFunc: function (e) { return e },
+  propNumber: 1,
+  propString: 'Hello World',
+
+  propObject: {
+    objectName1: '1',
+    objectName2: '2',
+    objectName3: '3'
   }
 }
 
