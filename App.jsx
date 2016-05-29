68f130c125aa78bbafa2426db0ba93a52f10a1a6
pawelduda <pawelduda91@gmail.com>
2016-05-29 18:31:59 +0200

    Simple example

--- App.jsx
+++ App.jsx
@@ -6,32 +6,20 @@ class App extends React.Component {
 
     this.state = {
       data: 'Initial data...'
-    }
+    };
 
     this.updateState = this.updateState.bind(this);
-  }
+  };
 
-  updateState(e) {
-    this.setState({data: e.target.value});
+  updateState() {
+    this.setState({data: 'Data updated...'});
   }
 
   render() {
     return (
       <div>
-        <Content myDataProp={this.state.data}
-          updateStateProp={this.updateState}></Content>
-      </div>
-    );
-  }
-}
-
-class Content extends React.Component {
-  render() {
-    return (
-      <div>
-        <input type="text" value={this.props.myDataProp}
-          onChange={this.props.updateStateProp} />
-        <h3>{this.props.myDataProp}</h3>
+        <button onClick={this.updateState}>CLICK</button>
+        <h4>{this.state.data}</h4>
       </div>
     );
   }
